bcd82091186a3e21d14cb6fb0a9a8aaec771cfbf
Brendan O'Brien <brendan.bobrien@gmail.com>
2013-07-29 15:46:28 -0700

    Code cleanup

--- js/main.js
+++ js/main.js
@@ -1,8 +1,3 @@
-//$('.idOpponents').click(
-//		function (){
-//			alert("hey");
-//			window.console.log($('.idOpponents'));
-//		});
 
 function f() {
 	var e = document.getElementById("idOpponents");
@@ -10,7 +5,6 @@ function f() {
 	var opTotal = document.getElementById("idOpponentTotal");
 	var opLast = document.getElementById("idOpponentLast");
         var oppScoreData = JSON.parse(window.sessionStorage.getItem('oppScoreData'));
-	//var opLastPoints = document.getElementById(e.options[e.selectedIndex].text).innerHTML;
         var opLastPoints = oppScoreData[e.options[e.selectedIndex].text];
 
 	opTotal.innerHTML = 'Opponent Total Points: '+ strUser;
